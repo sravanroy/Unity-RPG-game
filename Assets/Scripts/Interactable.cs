b77a95325650ff2220ca361279d95ed8a4f62303
sravanroy <sravanroy247@gmail.com>
2017-04-21 02:24:11 +0530

    Initial Commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    [HideInInspector]
+    public UnityEngine.AI.NavMeshAgent playerAgent;
+	private bool hasInteracted;
+	public virtual void MoveToInteraction(UnityEngine.AI.NavMeshAgent playerAgent) 
+	{
+	  hasInteracted=false;
+	  this.playerAgent=playerAgent;
+	  playerAgent.stoppingDistance=3f;
+	  playerAgent.destination=this.transform.position;
+
+	  
+	
+	}
+
+	void update()
+	{
+	    if(!hasInteracted && playerAgent!=null && !playerAgent.pathPending)
+		{
+		   if(playerAgent.remainingDistance<=playerAgent.stoppingDistance) 
+		   {
+		        Interact();
+		        hasInteracted=true;
+
+		   }
+		}
+
+
+	}
+
+	public virtual void Interact()
+
+	{
+		Debug.Log("Interacting with the base class");
+	}
+}
